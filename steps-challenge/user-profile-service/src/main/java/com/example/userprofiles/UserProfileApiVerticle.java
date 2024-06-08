ca644acb19f7292fe78be6cbe8dca35723778727
Zuo Yang <zuya@microsoft.com>
2024-06-08 10:38:46 +0800

    add user profile service

---
+++ steps-challenge/user-profile-service/src/main/java/com/example/userprofiles/UserProfileApiVerticle.java
@@ -0,0 +1,274 @@
+package com.example.userprofiles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.MaybeSource;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.mongo.MongoAuthenticationOptions;
+import io.vertx.ext.auth.mongo.MongoAuthorizationOptions;
+import io.vertx.rxjava3.core.AbstractVerticle;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.ext.auth.mongo.MongoAuthentication;
+import io.vertx.rxjava3.ext.auth.mongo.MongoUserUtil;
+import io.vertx.rxjava3.ext.mongo.MongoClient;
+import io.vertx.rxjava3.ext.web.Router;
+import io.vertx.rxjava3.ext.web.RoutingContext;
+import io.vertx.rxjava3.ext.web.handler.BodyHandler;
+
+import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
+
+public class UserProfileApiVerticle extends AbstractVerticle {
+
+	private static final int HTTP_PORT = 3000;
+	private static final Logger logger = LoggerFactory.getLogger(UserProfileApiVerticle.class);
+
+	private MongoClient mongoClient;
+	private MongoAuthentication authProvider;
+	private MongoUserUtil userUtil;
+
+	private JsonObject mongoConfig() {
+		return new JsonObject()
+				.put("host", "localhost")
+				.put("port", 27017)
+				.put("db_name", "profiles");
+	}
+
+	@Override
+	public Completable rxStart() {
+		mongoClient = MongoClient.createShared(vertx, mongoConfig());
+
+		authProvider = MongoAuthentication.create(mongoClient, new MongoAuthenticationOptions());
+		userUtil = MongoUserUtil.create(mongoClient, new MongoAuthenticationOptions(),
+				new MongoAuthorizationOptions());
+
+		Router router = Router.router(vertx);
+		BodyHandler bodyHandler = BodyHandler.create();
+		router.post().handler(bodyHandler);
+		router.put().handler(bodyHandler);
+		router.post("/register").handler(this::validateRegistration).handler(this::register);
+		router.get("/:username").handler(this::fetchUser);
+		router.put("/:username").handler(this::updateUser);
+		router.post("/authenticate").handler(this::authenticate);
+		router.get("/owns/:deviceId").handler(this::whoOwns);
+
+		return vertx.createHttpServer().requestHandler(router).rxListen(HTTP_PORT).ignoreElement();
+	}
+
+	private void validateRegistration(RoutingContext ctx) {
+		JsonObject body = jsonBody(ctx);
+		if (anyRegistrationFieldIsMissing(body) || anyRegistrationFieldIsWrong(body)) {
+			ctx.fail(400);
+		} else {
+			ctx.next();
+		}
+	}
+
+	private void register(RoutingContext ctx) {
+		JsonObject body = jsonBody(ctx);
+		String username = body.getString("username");
+		String password = body.getString("password");
+
+		JsonObject extraInfo = new JsonObject()
+				.put("$set", new JsonObject()
+						.put("email", body.getString("email"))
+						.put("city", body.getString("city"))
+						.put("deviceId", body.getString("deviceId"))
+						.put("makePublic", body.getBoolean("makePublic")));
+
+		userUtil
+				.rxCreateUser(username, password)
+				.flatMapMaybe(docId -> insertExtraInfo(extraInfo, docId))
+				.ignoreElement()
+				.subscribe(
+						() -> completeRegistration(ctx),
+						err -> handleRegistrationError(ctx, err));
+	}
+
+	private void fetchUser(RoutingContext ctx) {
+		String username = ctx.pathParam("username");
+
+		JsonObject query = new JsonObject()
+				.put("username", username);
+
+		JsonObject fields = new JsonObject()
+				.put("_id", 0)
+				.put("username", 1)
+				.put("email", 1)
+				.put("deviceId", 1)
+				.put("city", 1)
+				.put("makePublic", 1);
+
+		mongoClient
+				.rxFindOne("user", query, fields)
+				.toSingle()
+				.subscribe(
+						json -> completeFetchRequest(ctx, json),
+						err -> handleFetchError(ctx, err));
+	}
+
+	private void updateUser(RoutingContext ctx) {
+		String username = ctx.pathParam("username");
+		JsonObject body = jsonBody(ctx);
+
+		JsonObject query = new JsonObject().put("username", username);
+		JsonObject updates = new JsonObject();
+		if (body.containsKey("city")) {
+			updates.put("city", body.getString("city"));
+		}
+		if (body.containsKey("email")) {
+			updates.put("email", body.getString("email"));
+		}
+		if (body.containsKey("makePublic")) {
+			updates.put("makePublic", body.getBoolean("makePublic"));
+		}
+
+		if (updates.isEmpty()) {
+			ctx.response()
+					.setStatusCode(200)
+					.end();
+			return;
+		}
+		updates = new JsonObject()
+				.put("$set", updates);
+
+		mongoClient
+				.rxFindOneAndUpdate("user", query, updates)
+				.ignoreElement()
+				.subscribe(
+						() -> completeEmptySuccess(ctx),
+						err -> handleUpdateError(ctx, err));
+	}
+
+	private void authenticate(RoutingContext ctx) {
+		authProvider.rxAuthenticate(jsonBody(ctx))
+				.subscribe(
+						user -> completeEmptySuccess(ctx),
+						err -> handleAuthenticationError(ctx, err));
+	}
+
+	private void whoOwns(RoutingContext ctx) {
+		String deviceId = ctx.pathParam("deviceId");
+
+		JsonObject query = new JsonObject()
+				.put("deviceId", deviceId);
+
+		JsonObject fields = new JsonObject()
+				.put("_id", 0)
+				.put("username", 1)
+				.put("deviceId", 1);
+
+		mongoClient
+				.rxFindOne("user", query, fields)
+				.toSingle()
+				.subscribe(
+						json -> completeFetchRequest(ctx, json),
+						err -> handleFetchError(ctx, err));
+	}
+
+	private JsonObject jsonBody(RoutingContext ctx) {
+		if (ctx.body().length() == 0) {
+			return new JsonObject();
+		} else {
+			return ctx.body().asJsonObject();
+		}
+	}
+
+	private boolean anyRegistrationFieldIsMissing(JsonObject body) {
+		return !(body.containsKey("username") && body.containsKey("password")
+				&& body.containsKey("email") && body.containsKey("city")
+				&& body.containsKey("deviceId") && body.containsKey("makePublic"));
+	}
+
+	private final Pattern validUsername = Pattern.compile("\\w[\\w+|-]*");
+	private final Pattern validDeviceId = Pattern.compile("\\w[\\w+|-]*");
+
+	// Email regexp from
+	// https://www.owasp.org/index.php/OWASP_Validation_Regex_Repository
+	private final Pattern validEmail = Pattern.compile(
+			"^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+
+	private boolean anyRegistrationFieldIsWrong(JsonObject body) {
+		return !validUsername.matcher(body.getString("username")).matches()
+				|| !validEmail.matcher(body.getString("email")).matches()
+				|| body.getString("password").trim().isEmpty()
+				|| !validDeviceId.matcher(body.getString("deviceId")).matches();
+	}
+
+	private MaybeSource<? extends JsonObject> insertExtraInfo(JsonObject extraInfo, String docId) {
+		JsonObject query = new JsonObject().put("_id", docId);
+		return mongoClient
+				.rxFindOneAndUpdate("user", query, extraInfo)
+				.onErrorResumeNext(err -> {
+					return deleteIncompleteUser(query, err);
+				});
+	}
+
+	private MaybeSource<? extends JsonObject> deleteIncompleteUser(JsonObject query, Throwable err) {
+		if (isIndexViolated(err)) {
+			return mongoClient
+					.rxRemoveDocument("user", query)
+					.flatMap(del -> Maybe.error(err));
+		} else {
+			return Maybe.error(err);
+		}
+	}
+
+	private void completeRegistration(RoutingContext ctx) {
+		ctx.response().end();
+	}
+
+	private void handleRegistrationError(RoutingContext ctx, Throwable err) {
+		if (isIndexViolated(err)) {
+			logger.error("Registration failure: {}", err.getMessage());
+			ctx.fail(409);
+		} else {
+			fail500(ctx, err);
+		}
+	}
+
+	private void fail500(RoutingContext ctx, Throwable err) {
+		logger.error("Woops", err);
+		ctx.fail(500);
+	}
+
+	private boolean isIndexViolated(Throwable err) {
+		return err.getMessage().contains("E11000");
+	}
+
+	private void completeFetchRequest(RoutingContext ctx, JsonObject json) {
+		ctx.response()
+				.putHeader("Content-Type", "application/json")
+				.end(json.encode());
+	}
+
+	private void handleFetchError(RoutingContext ctx, Throwable err) {
+		if (err instanceof NoSuchElementException) {
+			ctx.fail(404);
+		} else {
+			fail500(ctx, err);
+		}
+	}
+
+	private void completeEmptySuccess(RoutingContext ctx) {
+		ctx.response().setStatusCode(200).end();
+	}
+
+	private void handleUpdateError(RoutingContext ctx, Throwable err) {
+		fail500(ctx, err);
+	}
+
+	private void handleAuthenticationError(RoutingContext ctx, Throwable err) {
+		logger.error("Authentication problem {}", err.getMessage());
+		ctx.response().setStatusCode(401).end();
+	}
+
+	public static void main(String[] args) {
+		Vertx.vertx().rxDeployVerticle(new UserProfileApiVerticle()).subscribe(
+				ok -> logger.info("HTTP server started on port " + HTTP_PORT),
+				err -> logger.error("Failed to start HTTP server", err));
+	}
+}
---
+++ steps-challenge/user-profile-service/src/test/java/com/example/userprofiles/IntegrationTest.java
@@ -0,0 +1,407 @@
+package com.example.userprofiles;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import io.reactivex.rxjava3.core.Maybe;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.IndexOptions;
+import io.vertx.ext.mongo.MongoClientDeleteResult;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.ext.mongo.MongoClient;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.with;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(VertxExtension.class)
+@DisplayName("User profile API integration tests")
+@Testcontainers
+class IntegrationTest {
+    @SuppressWarnings("resource")
+    @Container
+    private static final DockerComposeContainer<?> CONTAINERS = new DockerComposeContainer<>(
+            new File("src/test/resources/docker-compose.yaml"))
+            .withExposedService("mongo_1", 27017);
+
+    private static RequestSpecification requestSpecification;
+
+    @BeforeAll
+    static void prepareSpec() {
+        requestSpecification = new RequestSpecBuilder()
+                .addFilters(Arrays.asList(new ResponseLoggingFilter(), new RequestLoggingFilter()))
+                .setBaseUri("http://localhost:3000/")
+                .build();
+    }
+
+    private MongoClient mongoClient;
+
+    @BeforeEach
+    void setup(Vertx vertx, VertxTestContext testContext) {
+        JsonObject mongoConfig = new JsonObject()
+                .put("host", "localhost")
+                .put("port", 27017)
+                .put("db_name", "profiles");
+
+        mongoClient = MongoClient.createShared(vertx, mongoConfig);
+
+        mongoClient
+                .rxCreateIndexWithOptions("user", new JsonObject().put("username", 1),
+                        new IndexOptions().unique(true))
+                .andThen(mongoClient.rxCreateIndexWithOptions("user",
+                        new JsonObject().put("deviceId", 1),
+                        new IndexOptions().unique(true)))
+                .andThen(dropAllUsers())
+                .flatMapSingle(res -> vertx.rxDeployVerticle(new UserProfileApiVerticle()))
+                .subscribe(
+                        ok -> testContext.completeNow(),
+                        testContext::failNow);
+    }
+
+    private Maybe<MongoClientDeleteResult> dropAllUsers() {
+        return mongoClient.rxRemoveDocuments("user", new JsonObject());
+    }
+
+    @AfterEach
+    void cleanup(VertxTestContext testContext) {
+        dropAllUsers()
+                .doFinally(mongoClient::close)
+                .subscribe(
+                        ok -> testContext.completeNow(),
+                        testContext::failNow);
+    }
+
+    private JsonObject basicUser() {
+        return new JsonObject()
+                .put("username", "abc")
+                .put("password", "123")
+                .put("email", "abc@email.me")
+                .put("city", "Lyon")
+                .put("deviceId", "a1b2c3")
+                .put("makePublic", true);
+    }
+
+    @Test
+    @DisplayName("Register a user")
+    void register() {
+        String response = given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(basicUser().encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .asString();
+        assertThat(response).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Failing to register with an existing user name")
+    void registerExistingUser() {
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(basicUser().encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(basicUser().encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(409);
+    }
+
+    @Test
+    @DisplayName("Failing to register a user with an already existing device id")
+    void registerExistingDeviceId(VertxTestContext testContext) {
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(basicUser().encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+        JsonObject user = basicUser()
+                .put("username", "Bean");
+
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(user.encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(409);
+
+        mongoClient
+                .rxFindOne("user", new JsonObject().put("username", "Bean"), new JsonObject())
+                .subscribe(
+                        found -> testContext.failNow(
+                                new IllegalStateException("Incomplete document still inserted: " + found.encode())),
+                        testContext::failNow,
+                        testContext::completeNow);
+    }
+
+    @Test
+    @DisplayName("Failing to register with missing fields")
+    void registerWIthMissingFields() {
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(new JsonObject().encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Failing to register with incorrect field data")
+    void registerWithWrongFields() {
+        JsonObject user = basicUser().put("username", "a b c ");
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(user.encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(400);
+
+        user = basicUser().put("deviceId", "@123");
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(user.encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(400);
+
+        user = basicUser().put("password", " ");
+        given()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(user.encode())
+                .when()
+                .post("/register")
+                .then()
+                .assertThat()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Register a user then fetch it")
+    void registerThenFetch() {
+        JsonObject user = basicUser();
+
+        with()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(user.encode())
+                .post("/register");
+
+        JsonPath jsonPath = given()
+                .spec(requestSpecification)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/" + user.getString("username"))
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+
+        assertThat(jsonPath.getString("username")).isEqualTo(user.getString("username"));
+        assertThat(jsonPath.getString("email")).isEqualTo(user.getString("email"));
+        assertThat(jsonPath.getString("deviceId")).isEqualTo(user.getString("deviceId"));
+        assertThat(jsonPath.getString("city")).isEqualTo(user.getString("city"));
+        assertThat(jsonPath.getBoolean("makePublic")).isEqualTo(user.getBoolean("makePublic"));
+        assertThat(jsonPath.getString("_id")).isNull();
+        assertThat(jsonPath.getString("password")).isNull();
+    }
+
+    @Test
+    @DisplayName("Fetching an unknown user")
+    void fetchUnknownUser() {
+        given()
+                .spec(requestSpecification)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/foo-bar-baz")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("Register then update a user data")
+    void update() {
+        JsonObject original = basicUser();
+
+        with()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(original.encode())
+                .post("/register");
+
+        JsonObject updated = basicUser();
+        updated
+                .put("deviceId", "vertx-in-action-123")
+                .put("email", "vertx@email.me")
+                .put("city", "Nevers")
+                .put("makePublic", false)
+                .put("username", "Bean");
+
+        with()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .body(updated.encode())
+                .put("/" + original.getString("username"))
+                .then()
+                .statusCode(200);
+
+        JsonPath jsonPath = given()
+                .spec(requestSpecification)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/" + original.getString("username"))
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+
+        assertThat(jsonPath.getString("username")).isEqualTo(original.getString("username"));
+        assertThat(jsonPath.getString("deviceId")).isEqualTo(original.getString("deviceId"));
+
+        assertThat(jsonPath.getString("city")).isEqualTo(updated.getString("city"));
+        assertThat(jsonPath.getString("email")).isEqualTo(updated.getString("email"));
+        assertThat(jsonPath.getBoolean("makePublic")).isEqualTo(updated.getBoolean("makePublic"));
+    }
+
+    @Test
+    @DisplayName("Authenticate an existing user")
+    void authenticate() {
+        JsonObject user = basicUser();
+        JsonObject request = new JsonObject()
+                .put("username", user.getString("username"))
+                .put("password", user.getString("password"));
+
+        with()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(user.encode())
+                .post("/register");
+
+        with()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .body(request.encode())
+                .post("/authenticate")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Failing at authenticating an unknown user")
+    void authenticateMissingUser() {
+        JsonObject request = new JsonObject()
+                .put("username", "Bean")
+                .put("password", "abc");
+
+        with()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .body(request.encode())
+                .post("/authenticate")
+                .then()
+                .assertThat()
+                .statusCode(401);
+    }
+
+    @Test
+    @DisplayName("Find who owns a device")
+    void whoHas() {
+        JsonObject user = basicUser();
+
+        with()
+                .spec(requestSpecification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(user.encode())
+                .post("/register");
+
+        JsonPath jsonPath = given()
+                .spec(requestSpecification)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/owns/" + user.getString("deviceId"))
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .jsonPath();
+
+        assertThat(jsonPath.getString("username")).isEqualTo(user.getString("username"));
+        assertThat(jsonPath.getString("deviceId")).isEqualTo(user.getString("deviceId"));
+        assertThat(jsonPath.getString("city")).isNull();
+
+        given()
+                .spec(requestSpecification)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/owns/404")
+                .then()
+                .assertThat()
+                .statusCode(404);
+    }
+}
